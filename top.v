dade0f870e42a73f6ed95c3e12136b5971a7e08b
Tessa Downs <156485194+tessadowns@users.noreply.github.com>
2025-02-04 16:48:48 -0700

    Add files via upload

--- top.v
+++ top.v
@@ -1,30 +1,10 @@
 module top(
-    input [3:0] sw,
-    output [2:0] led
+    input [15:0] sw,
+    output [15:0] led
 );
 
-   (* keep_hierarchy = "yes" *)  naive naive_inst(
-        .A(sw[3]),
-        .B(sw[2]),
-        .C(sw[1]),
-        .D(sw[0]),
-        .Y(led[0])
-    );
-
-  (* keep_hierarchy = "yes" *)  minterm minterm_inst(
-        .A(sw[3]),
-        .B(sw[2]),
-        .C(sw[1]),
-        .D(sw[0]),
-        .Y(led[1])
-    );
-
-   (* keep_hierarchy = "yes" *)  maxterm maxterm_inst(
-        .A(sw[3]),
-        .B(sw[2]),
-        .C(sw[1]),
-        .D(sw[0]),
-        .Y(led[2])
-    );
+    assign led[0] = sw[0];
+    assign led[1]= sw[1] | sw[2];
+    assign led[2]= sw[1] & sw[2];
 
 endmodule
